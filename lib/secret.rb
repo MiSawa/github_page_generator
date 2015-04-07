928293a4b2865d256bb11d8cf7a772ba6fe873c8
Mi_Sawa <mi.sawa.1216+git@gmail.com>
2015-04-08 01:46:19 +0900

    secret.rb を, secret/ に移すのではなく ignore するように変更.

--- lib/secret.rb
+++ lib/secret.rb
@@ -1,20 +1,30 @@
 
 class SecretPageExtension < Middleman::Extension
   #option :enable_secret, true, 'Enable secret mode'
-  #TODO: '/secret/{path}' をオプション化
-  option :secret_link, '/secret/{path}', 'Path secret pages are generated at.'
+  #option :secret_link, '/secret/{path}', 'Path secret pages are generated at.'
   option :secret_tag, 'secret', 'Frontmatter tag for secret pages.'
   def manipulate_resource_list(resources)
-    resources.map do |p|
-      if p.data[options.secret_tag]
-        #'secret/' + p.destination_path
-        p.destination_path = Middleman::Blog::UriTemplates::apply_uri_template(
-          Middleman::Blog::UriTemplates::uri_template(options.secret_link),
-          path: p.destination_path
-        )
-      end
-      p
+    #return resources.reject do |resource|
+    #  resource.data[options.secret_tag]
+    #end
+    for resource in resources
+      @app.ignore resource.path if resource.data[options.secret_tag]
     end
+    return resources
+    #return resources.reject do |resource|
+    #  resource.data[options.secret_tag]
+    #end
+    #resources.map do |resource|
+    #  if resource.data[options.secret_tag]
+    #    @app.ignore resource.path
+    #    #'secret/' + p.destination_path
+    #    resource.destination_path = Middleman::Blog::UriTemplates::apply_uri_template(
+    #      Middleman::Blog::UriTemplates::uri_template(options.secret_link),
+    #      path: resource.destination_path
+    #    )
+    #  end
+    #  resource
+    #end
   end
 end
 
