c844b8e1b29da17cb35dced3bea32d1e6a49087f
mi_sawa <mi.sawa.1216+git@gmail.com>
2020-06-13 04:14:05 +0900

    Styling

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ function get_this_dir() {
 
 readonly this_dir="$(get_this_dir)"
 
-docker run -it --rm -v"${this_dir}":'/app' -w '/app' -p4567:4567 'ghgen' bundle exec middleman build --verbose 2>&1 | awk '!/URI.(un)?escape is obsolete/'
+docker run -it --rm -v"${this_dir}":'/app' -w '/app' 'ghgen' bundle exec middleman build --verbose 2>&1 | awk '!/URI.(un)?escape is obsolete/'
